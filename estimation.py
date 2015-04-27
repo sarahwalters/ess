d1b50ae0df9650f46b5b545aef9a21dd002f1abc
Sarah Walters <sarah.walters@students.olin.edu>
2015-04-27 11:59:37 -0400

    Refactoring estimation - use class

--- estimation.py
+++ estimation.py
@@ -22,18 +22,23 @@ def plotAll():
 		estimateMean(col)
 
 		# diff in means compared to unaffiliated, for each religion
+		diffMeansEsts = []
 		for i in range(len(rlgdfs)):
-			estimateDiffMeans(col, rlgdfs[i], rlglabels[i])
+			est = estimateDiffMeans(col, rlgdfs[i], rlglabels[i])
+			diffMeansEsts.append(est)
 
 		# plot all diff means together
-		estimateDiffMeans_all(col, rlgdfs, rlglabels)
+		diffMeansPlot(col, diffMeansEsts)
+
+def debug():
+	rlgLookup = [(1, 'Catholic'), (2, 'Protestant'), (5, 'Jewish'), (6, 'Islamic')]
+	rlgdfs = [df[df.rlgdnm==tup[0]] for tup in rlgLookup]
+	rlglabels = [tup[1] for tup in rlgLookup]
+
+	estimateMean('ipcrtiv')
 
 
 def estimateMean(col, n=10, m=1000, show=False):
-	# for plotting confidence interval & mean
-	def VertLine(x, y=1, color='0.8'):
-		thinkplot.Plot([x, x], [0, y], color=color, linewidth=3)
-
 	# get data
 	data = unaffiliated[col] # length: ~2e6
 
@@ -41,40 +46,18 @@ def estimateMean(col, n=10, m=1000, show=False):
 	mu = data.mean()
 	sigma = data.std()
 	
+	# make labels
+	xlabel='Estimated mean of ' + col + ' among unaffiliated respondents'
+	savepath = 'plots/estimation/mean/'+ col
+
 	# run "experiments"
-	means = []
-	for _ in range(m):
-		xs = [random.gauss(mu,sigma) for _ in range(n)]
-		xbar = np.mean(xs)
-		means.append(xbar)
+	est = GaussianEstimate(mu, sigma, m, n, xlabel, savepath)
+	est.plot(show=show)
 
-	# sampling CDF & summary statistics
-	samplingCdf = thinkstats2.Cdf(means, label='sampling')
-	confInt = samplingCdf.ConfidenceInterval()
-
-	printMean = 'Sample mean: ' + str(round(mu,3))
-	printConfInt = 'Confidence interval: [' + str(round(confInt[0], 3)) + ', ' + str(round(confInt[1], 3)) + ']'
-	printSE = 'Standard error: ' + str(round(RMSE(means, mu), 3))
-
-	title = printMean + ' \n ' + printConfInt + ' , ' + printSE
-
-	thinkplot.Config(xlabel='Estimated mean of ' + col + ' among unaffiliated respondents', ylabel='CDF', title=title)
-	VertLine(confInt[0])
-	VertLine(confInt[1])
-	VertLine(mu, color='0.2')
-	thinkplot.Cdf(samplingCdf)
-
-	if show:
-		thinkplot.Show()
-	else:
-		thinkplot.Save('plots/estimation/mean/'+ col, formats=['jpg'])
+	return est
 
 
 def estimateDiffMeans(col, rlgdf, rlglabel, n=10, m=1000, show=False):
-	# for plotting confidence interval & mean
-	def VertLine(x, y=1, color='0.8'):
-		thinkplot.Plot([x, x], [0, y], color=color, linewidth=3)
-
 	# get data
 	dataReligion = rlgdf[col] # length: ~2e6
 	dataUnaffiliated = unaffiliated[col]
@@ -84,37 +67,23 @@ def estimateDiffMeans(col, rlgdf, rlglabel, n=10, m=1000, show=False):
 	sigma = (dataReligion.std()**2/len(dataReligion) + dataUnaffiliated.std()**2/len(dataUnaffiliated))**0.5
 	# standard dev. from http://www.kean.edu/~fosborne/bstat/06b2means.html
 
+	# make labels
+	xlabel = 'Estimated ' + rlglabel + '/unaffiliated diff in means of ' + col
+	savepath = 'plots/estimation/diffMeans/' + col + '_' + rlglabel
+
 	# run "experiments"
-	means = []
-	for _ in range(m):
-		xs = [random.gauss(mu,sigma) for _ in range(n)]
-		xbar = np.mean(xs)
-		means.append(xbar)
+	est = GaussianEstimate(mu, sigma, m, n, xlabel, savepath)
+	est.plot(show=show)
 
-	# sampling CDF & summary statistics
-	samplingCdf = thinkstats2.Cdf(means, label='sampling')
-	confInt = samplingCdf.ConfidenceInterval()
-
-	printMean = 'Sample mean: ' + str(round(mu,3))
-	printConfInt = 'Confidence interval: [' + str(round(confInt[0], 3)) + ', ' + str(round(confInt[1], 3)) + ']'
-	printSE = 'Standard error: ' + str(round(RMSE(means, mu), 3))
-
-	title = printMean + ' \n ' + printConfInt + ' , ' + printSE
-
-	thinkplot.Config(xlabel='Estimated ' + rlglabel + '/unaffiliated diff in means of ' + col, ylabel='CDF', title=title)
-	VertLine(confInt[0])
-	VertLine(confInt[1])
-	VertLine(mu, color='0.2')
-	thinkplot.Cdf(samplingCdf)
-
-	if show:
-		thinkplot.Show()
-	else:
-		thinkplot.Save('plots/estimation/diffMeans/' + col + '_' + rlglabel, formats=['jpg'])
+	return est
 
 
-class Estimate:
-	def __init__(self, mu, sigma, m, n):
+class GaussianEstimate:
+	def __init__(self, mu, sigma, m, n, xlabel, savepath):
+		# labels
+		self.xlabel = xlabel
+		self.savepath = savepath
+
 		# estimators
 		self.mu = mu
 		self.sigma = sigma
@@ -127,64 +96,49 @@ class Estimate:
 			self.means.append(xbar)
 
 		# make sampling cdf
-		self.samplingCdf = thinkstats2.Cdf(means, label='sampling')
-		self.confInt = samplingCdf.ConfidenceInterval()
+		self.samplingCdf = thinkstats2.Cdf(self.means, label='dummy label')
+		self.confInt = self.samplingCdf.ConfidenceInterval()
 
 		# for displaying nicely
 		self.printMean = 'Sample mean: ' + str(round(mu,3))
 		self.printConfInt = 'Confidence interval: [' + str(round(self.confInt[0], 3)) + ', ' + str(round(self.confInt[1], 3)) + ']'
 		self.printSE = 'Standard error: ' + str(round(RMSE(self.means, mu), 3))
 
+		# give cdf proper label
+		self.title = self.printMean + ' \n ' + self.printConfInt + ' , ' + self.printSE
+		self.samplingCdf.label = self.title
 
-
-def estimateDiffMeans_all(col, rlgdfs, rlglabels, n=10, m=1000, show=False):
-	# get data
-	rlgSeriesArray = [rlgdf[col] for rlgdf in rlgdfs]
-	unaffSeries = unaffiliated[col]
-
-	# estimators
-	muArray = [rs.mean() - unaffSeries.mean() for rs in rlgSeriesArray]
-	sigmaArray = [(rs.std()**2/len(rs) + unaffSeries.std()**2/len(unaffSeries))**0.5 for rs in rlgSeriesArray]
-	# standard dev. from http://www.kean.edu/~fosborne/bstat/06b2means.html
 	
-	# run "experiments"
-	samplingCdfs = []
-	for i in range(len(muArray)):
-		mu = muArray[i]
-		sigma = sigmaArray[i]
+	def plot(self, show=False):
+		thinkplot.Clf()
 
-		means = []
-		for _ in range(m):
-			xs = [random.gauss(mu,sigma) for _ in range(n)]
-			xbar = np.mean(xs)
-			means.append(xbar)
+		thinkplot.Config(xlabel=self.xlabel, ylabel='CDF', title=self.title, legend=False)
 
-		# sampling CDF & summary statistics
-		# ... for making the label
-		samplingCdf = thinkstats2.Cdf(means, label='dummy title')
-		
-		# ... make the label		
-		confInt = samplingCdf.ConfidenceInterval()
-		printMean = 'Mean: ' + str(round(mu,3))
-		printConfInt = 'CI: [' + str(round(confInt[0], 3)) + ', ' + str(round(confInt[1], 3)) + ']'
-		printSE = 'SE: ' + str(round(RMSE(means, mu), 3))
+		VertLine(self.confInt[0])
+		VertLine(self.confInt[1])
+		VertLine(self.mu, color='0.2')
 
-		title = rlglabels[i] + ' = ' + printMean + ', ' + printConfInt + ' , ' + printSE
-		
-		# ...assign the label
-		samplingCdf = thinkstats2.Cdf(means, label=title) # real cdf with label
-		samplingCdfs.append(samplingCdf)
+		thinkplot.Cdf(self.samplingCdf)
 
+		if show:
+			thinkplot.Show()
+		else:
+			thinkplot.Save(self.savepath, formats=['jpg'])
+
+
+def diffMeansPlot(col, diffMeansEsts, show=False):
 	thinkplot.Config(xlabel='Estimated religious/unaffiliated diff in means of ' + col, ylabel='CDF', loc='lower center', bbox_to_anchor=(0.5, -0.6), htscale=0.825)
 	if col in values:
 		thinkplot.Config(xlim=(-1.25,1.25))
 
+	samplingCdfs = [est.samplingCdf for est in diffMeansEsts]
 	thinkplot.Cdfs(samplingCdfs)
 
 	if show:
 		thinkplot.Show()
 	else:
-		thinkplot.Save('plots/estimation/diffMeans/' + col + '_all', formats=['jpg'])
+		thinkplot.Save('plots/estimation/diffMeansAll/' + col, formats=['jpg'])
+
 
 # from ThinkStats2
 def RMSE(estimates, actual):
@@ -198,3 +152,6 @@ def RMSE(estimates, actual):
     e2 = [(estimate-actual)**2 for estimate in estimates]
     mse = np.mean(e2)
     return math.sqrt(mse)
+
+def VertLine(x, y=1, color='0.8'):
+		thinkplot.Plot([x, x], [0, y], color=color, linewidth=3)
