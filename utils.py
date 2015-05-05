d1c4eeb71869f96718f13dd2470dcac523711020
Sarah Walters <sarah.walters@students.olin.edu>
2015-05-05 12:14:49 -0400

    Centering values

--- utils.py
+++ utils.py
@@ -7,3 +7,15 @@ def getCodeList(csvpath):
 		for row in reader:
 			res += row
 	return res
+
+def getCountryDict(csvpath='data/codeinfo/countryLabels.csv'):
+	countryDict = {}
+	with open(csvpath) as csvfile:
+		reader = csv.reader(csvfile, delimiter=' ')
+		for row in reader:
+			countryDict[row[0]] = row[1]
+
+	lengths = [len(v) for v in countryDict.values()]
+	countryDict['longest'] = max(lengths)
+
+	return countryDict
